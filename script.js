94b6a3de9487424e7dd86166f205fb3cc5539ace
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-25 17:30:10 +0100

    feat: dati utente

---
+++ script.js
@@ -0,0 +1,35 @@
+// Chiedere i dati all'utente
+const userKm = parseFloat (prompt('Totalekm'));
+const userAge = prompt('Età');
+
+
+
+
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+//     - prezzo biglietto = 0,21€ * 1Km 
+
+
+
+
+
+//     - lo sconto iniziale è pari a 0 
+
+
+
+
+
+// - va applicato uno sconto del 20% per i minorenni
+//     - sconto per le persone con età < 18 anni = 20%
+
+
+
+
+
+// - va applicato uno sconto del 40% per gli over 65
+//     - sconto per le persone con età > 65 anni = 40%
+
+
+
+
+    
+// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
