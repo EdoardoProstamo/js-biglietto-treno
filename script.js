f6db52729a273b1bcb774314552911aed138346e
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-02-25 18:08:06 +0100

    feat: prezzi scontati

--- script.js
+++ script.js
@@ -16,20 +16,15 @@ let Sale = 0
 
 // - va applicato uno sconto del 20% per i minorenni
 //     - sconto per le persone con età < 18 anni = 20%
+// // - va applicato uno sconto del 40% per gli over 65
+//     - sconto per le persone con età > 65 anni = 40%
 if (userAge<18) {
     Sale = 20;
-}
-
-
-
-
-// - va applicato uno sconto del 40% per gli over 65
-//     - sconto per le persone con età > 65 anni = 40%
-if (userAge>65) {
+} else if (userAge>65) {
     Sale = 40;
 }
+const NewPrice = TotalPrice - ((TotalPrice * Sale) / 100);
+console.log(NewPrice);
+  
+// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
 
-
-
-    
-// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
